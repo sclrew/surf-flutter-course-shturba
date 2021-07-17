8875bd996a4e64314872bd671757f7c6deb6ee9d
sclrew <sclrew@yahoo.com>
2021-07-17 23:43:05 +0300

    4.2 scaffold

---
+++ places/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'ui/screen/sight_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SightListScreen(),
+    );
+  }
+}
