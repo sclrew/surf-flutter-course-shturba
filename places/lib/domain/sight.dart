9a105b004398f71fd388849ef772b3ed2507f8be
sclrew <sclrew@yahoo.com>
2021-07-23 12:36:23 +0300

    добавил интересные места

---
+++ places/lib/domain/sight.dart
@@ -0,0 +1,12 @@
+enum MyTypes { nature, history, interes }
+
+class Sight {
+  String name;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  MyTypes type;
+
+  Sight(this.name, this.lat, this.lon, this.url, this.details, this.type);
+}
