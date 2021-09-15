4dfc862a07ad1363b73e410d927f6f3e8bfb7fcd
sclrew <sclrew@yahoo.com>
2021-09-15 07:14:50 +0300

    finished

--- places/lib/ui/screen/onboardingscreen.dart
+++ places/lib/ui/screen/onboardingscreen.dart
@@ -270,7 +270,6 @@ class CustomSliderThumbRect extends SliderComponentShape {
     final paint = Paint()
       ..color = const Color(0xff4CAF50)
       ..style = PaintingStyle.fill;
-    // ..strokeWidth = 2.0;
 
     final path = Path();
     final myRect = Rect.fromLTWH(center.dx - 12, center.dy - 4, thumbRadius, 8);
